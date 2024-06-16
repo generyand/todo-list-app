f80f658d384eb3a482b0f440dffd0817fe2c62ce
generyand <generyan.dep@gmail.com>
2024-06-16 17:13:25 +0800

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 import Header from "./components/Header";
 import { useLocalStorage } from "./utils/useLocalStorage";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   const [todos, setTodos] = useState([]);
