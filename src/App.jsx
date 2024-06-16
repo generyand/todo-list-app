163f4d8eec17dd69ee4c0ea908bb21cfa8ca98f3
generyand <generyan.dep@gmail.com>
2024-06-16 18:14:19 +0800

    removed fragment in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,17 +32,15 @@ function App() {
   }, []);
 
   return (
-    <>
-      <main className="main | max-w-[32rem] mx-auto sm:mt-[10vh] bg-white sm:rounded-md h-screen w-full md:w-auto md:h-auto py-4 overflow-y-scroll sm:max-h-[40rem]">
-        <Header />
-        <TodoInput onAddTask={handleAddTask} />
-        <TodoList
-          setTodos={setTodos}
-          todos={todos}
-          onDeleteTask={handleDeleteTask}
-        />
-      </main>
-    </>
+    <main className="main | max-w-[32rem] mx-auto sm:mt-[10vh] bg-white sm:rounded-md h-screen w-full md:w-auto md:h-auto py-4 overflow-y-scroll sm:max-h-[40rem]">
+      <Header />
+      <TodoInput onAddTask={handleAddTask} />
+      <TodoList
+        setTodos={setTodos}
+        todos={todos}
+        onDeleteTask={handleDeleteTask}
+      />
+    </main>
   );
 }
 
