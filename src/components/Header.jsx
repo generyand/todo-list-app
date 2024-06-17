5ab22f99a234ecd01037c3d4d329de26a49e3f88
generyand <generyan.dep@gmail.com>
2024-06-17 10:10:35 +0800

    minor changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="absolute flex items-center gap-2 p-2 mb-2 text-xl font-bold text-gray-700 text-end md:text-2xl md:gap-5">
+    <header className="absolute flex items-center w-full gap-2 p-2 mb-2 text-xl font-bold text-gray-700 text-end md:text-2xl md:gap-5">
       <Bars3BottomLeftIcon className="w-6 h-6 " />
-      <h1 className="">Gotta<span className="text-sky-500">Dos</span></h1>
+      <h1 className="">
+        Gotta<span className="text-sky-500">Dos</span>
+      </h1>
     </header>
   );
 }
