e6fd09d15e8977134e84d95ffda70faf3431e51b
generyand <generyan.dep@gmail.com>
2024-06-17 10:05:37 +0800

    changed logo color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="absolute flex items-center gap-2 p-2 mb-2 text-xl font-bold text-gray-700 text-end md:text-2xl md:gap-5">
       <Bars3BottomLeftIcon className="w-6 h-6 " />
-      <h1 className="">GottaDos</h1>
+      <h1 className="">Gotta<span className="text-sky-500">Dos</span></h1>
     </header>
   );
 }
