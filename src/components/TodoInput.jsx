ed4b7fd88abfdba5d3fe59631fdb5a4d372f70cc
generyand <generyan.dep@gmail.com>
2024-06-17 15:16:52 +0800

    minor changes in dark mode

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -12,7 +12,10 @@ export default function TodoInput({ onAddTask }) {
   };
 
   return (
-    <form onSubmit={handleAddTask} className="px-3 mb-5 border-b mt-[5rem] md:px-4 dark:text-white">
+    <form
+      onSubmit={handleAddTask}
+      className="px-3 mb-5 border-b dark:border-b-sky-700 mt-[5rem] md:px-4 dark:text-white"
+    >
       <div className="flex items-center justify-between w-full gap-4 px-3 py-2 mb-4 leading-tight text-gray-700 bg-white border rounded dark:border-sky-700 dark:bg-sky-800">
         <input
           value={newTask}
